468be8ca6161e809b61d90e1e2230da826bf974c
FERHATSUMER <sumerf39@gmail.com>
2023-10-26 15:34:20 +0300

    entrance-redux

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { decrement, increment } from "./pages/counter/CounterSlice";
+
+function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  return (
+    <div className="App">
+      <div>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
